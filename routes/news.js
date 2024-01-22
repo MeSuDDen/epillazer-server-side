9a5435e6480aac23d810db8622e37468d0bfa9f3
SuDDen <workdev.sudden@gmail.com>
2024-01-23 09:33:38 +1000

    Add dev branch

--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 
 const router = express.Router()
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
 	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PASS,
